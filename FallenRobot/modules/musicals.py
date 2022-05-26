eefa9aeffb96479cc2646ab3fafc1c8f6509bd9e
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-26 15:29:22 +0530

    Update musicals.py

--- FallenRobot/modules/musicals.py
+++ FallenRobot/modules/musicals.py
@@ -3,4 +3,6 @@ __mod_name__ = "Mᴜsɪᴄ"
 __help__ = """
  ❍ /song <song name>*:* Uploads the song in it's best quality available
  💡Ex: `/song Faded Alan Walker`
+   ᴍᴏɪ ɴᴇᴛᴡᴏʀᴋ:- @Mastermind\_network\_official
+   ᴅᴇᴠᴇʟᴏᴘᴇʀ :-  @iTz\_mSt_bOy
 """
