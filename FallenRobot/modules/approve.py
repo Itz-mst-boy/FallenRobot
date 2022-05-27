cf174d460c36a4cf242eed8eaa41f71172218bf6
ᴍᴜᴋᴇsʜ <100356452+Itz-mst-boy@users.noreply.github.com>
2022-05-27 10:55:47 +0530

    Update approve.py

--- FallenRobot/modules/approve.py
+++ FallenRobot/modules/approve.py
@@ -209,6 +209,7 @@ That's what approvals are for - approve of trustworthy users to allow them to se
 ❍ /unapprove*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
 ❍ /approved*:* List all approved users.
 ❍ /unapproveall*:* Unapprove *ALL* users in a chat. This cannot be undone.
+  ᴍᴏɪ ɴᴇᴛᴡᴏʀᴋ :- @mastermind\_network\_official
 """
 
 APPROVE = DisableAbleCommandHandler("approve", approve)
